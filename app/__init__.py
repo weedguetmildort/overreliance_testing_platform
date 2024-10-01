55744fa775006c97100297cd74c47f072c1a1d0f
Weedguet Mildort <108565362+weedguetmildort@users.noreply.github.com>
2024-10-01 11:58:33 -0400

    live testing

--- app/__init__.py
+++ app/__init__.py
@@ -1,16 +1,25 @@
-import os
 from flask import Flask
 
-def create_app():
-    app = Flask(__name__)
 
-    app.secret_key = 'griffin'
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "Flash Heroku app."
+
+if __name__ == '__main__':
+    app.run
+
+# def create_app():
+#     app = Flask(__name__)
+
+#     app.secret_key = 'griffin'
     
-    # Load configuration
-    # app.config.from_object('config.Config')
+#     # Load configuration
+#     # app.config.from_object('config.Config')
 
-    # Register blueprints
-    from .routes import main_bp
-    app.register_blueprint(main_bp)
+#     # Register blueprints
+#     from .routes import main_bp
+#     app.register_blueprint(main_bp)
 
-    return app
+#     return app
