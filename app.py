1a5967c678b228814e6580cb4a3c1a04e054d4ea
Weedguet Mildort <108565362+weedguetmildort@users.noreply.github.com>
2024-10-01 12:30:34 -0400

    update

--- app.py
+++ app.py
@@ -1,10 +1,9 @@
 from flask import Flask
 
-app = Flask(__name__)
+from app import app
 
-@app.route('/')
-def home():
-    return "Flask heroku app."
+
+app = app
 
 if __name__ == "__main__":
     app.run()
